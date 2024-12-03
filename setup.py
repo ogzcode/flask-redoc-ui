bfbc78d3b8625d8c20166b6fdaead0f5a844d360
ogzCode <oguzguc44@gmail.com>
2024-12-03 16:36:55 +0300

    Enhance setup.py with project metadata and long description

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup, find_packages
 
+
+with open('README.md', 'r', encoding="utf-8") as file:
+    long_description = file.read()  
+
 setup(
     name='flask_pydantic_redoc',
     version='0.1.0',
+    url='https://github.com/ogzcode/flask-pydantic-redoc',
+    author='Oğuzhan Güç(ogzCode)',
+    author_email='oguzguc44@gmail.com',
     packages=find_packages(),
-    include_package_data=True,
+    description="Flask extension for generating OpenAPI documentation with Pydantic models and ReDoc UI",
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
     install_requires=[
         'flask',
         'apispec',
@@ -13,18 +22,14 @@ setup(
     ],
     extras_require={
         'test': [
-            'pytest',
-            'pytest-flask',
+            'pytest'
         ],
     },
-    package_data={
-        'flask_redoc': ['templates/*.html'],
-    },
     classifiers=[
-        'Framework :: Flask',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
